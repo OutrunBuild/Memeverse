6abb58fb5124a6cf261e9f90437f1ded99291038
Jason <2045330878@qq.com>
2024-11-30 04:24:56 +0800

    make liquidproof omnichain

--- src/token/MemeLiquidProof.sol
+++ src/token/MemeLiquidProof.sol
@@ -1,21 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.26;
 
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+import { OutrunOFT } from "./OutrunOFT.sol";
 import { IMemeLiquidProof } from "./interfaces/IMemeLiquidProof.sol";
 
 /**
- * @title Memeverse Liquidity proof Token
+ * @title Omnichain Memeverse Liquidity Proof Token
  */
-contract MemeLiquidProof is IMemeLiquidProof {
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    uint256 public totalSupply;
-    address public memecoin;
-    address public memeverseLauncher;
-
-    mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
+contract MemeLiquidProof is IMemeLiquidProof, OutrunOFT {
+    address public immutable memecoin;
+    address public immutable memeverseLauncher;
 
     modifier onlyMemeverseLauncher() {
         require(msg.sender == memeverseLauncher, PermissionDenied());
@@ -27,41 +23,14 @@ contract MemeLiquidProof is IMemeLiquidProof {
         string memory _symbol, 
         uint8 _decimals, 
         address _memecoin, 
-        address _memeverseLauncher
-    ) {
-        name = _name;
-        symbol = _symbol;
-        decimals = _decimals;
+        address _memeverseLauncher,
+        address _lzEndpoint,
+        address _delegate
+    ) OutrunOFT(_name, _symbol, _decimals, _lzEndpoint, _delegate) Ownable(_delegate) {
         memecoin = _memecoin;
         memeverseLauncher = _memeverseLauncher;
     }
     
-    function approve(address spender, uint256 amount) public returns (bool) {
-        allowance[msg.sender][spender] = amount;
-
-        emit Approval(msg.sender, spender, amount);
-        return true;
-    }
-
-    function transfer(address to, uint256 amount) public returns (bool) {
-        address msgSender = msg.sender;
-        _transfer(msgSender, to, amount);
-
-        return true;
-    }
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 amount
-    ) public returns (bool) {
-        uint256 allowed = allowance[from][msg.sender];
-        if (allowed != type(uint256).max) allowance[from][msg.sender] = allowed - amount;
-        _transfer(from, to, amount);
-
-        return true;
-    }
-
     function mint(address account, uint256 amount) external override onlyMemeverseLauncher {
         _mint(account, amount);
     }
@@ -69,41 +38,5 @@ contract MemeLiquidProof is IMemeLiquidProof {
     function burn(address account, uint256 amount) external onlyMemeverseLauncher {
         require(balanceOf[account] >= amount, InsufficientBalance());
         _burn(account, amount);
-    }
-
-    function _transfer(address from, address to, uint256 amount) internal {
-        balanceOf[from] -= amount;
-
-        unchecked {
-            balanceOf[to] += amount;
-        }
-
-        if (to == address(0)) {
-            unchecked {
-                totalSupply -= amount;
-            }
-        }
-
-        emit Transfer(from, to, amount);
-    }
-
-    function _mint(address to, uint256 amount) internal {
-        totalSupply += amount;
-
-        unchecked {
-            balanceOf[to] += amount;
-        }
-
-        emit Transfer(address(0), to, amount);
-    }
-
-    function _burn(address from, uint256 amount) internal {
-        balanceOf[from] -= amount;
-
-        unchecked {
-            totalSupply -= amount;
-        }
-
-        emit Transfer(from, address(0), amount);
     }
 }

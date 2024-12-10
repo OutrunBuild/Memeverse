97893158ceb0a3c6622ce0ccd1a4d8cd70109e47
Jason <2045330878@qq.com>
2024-12-10 17:51:35 +0800

    update optimizer runs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -44,7 +44,7 @@ const config: HardhatUserConfig = {
                 settings: {
                     optimizer: {
                         enabled: true,
-                        runs: 100000,
+                        runs: 10000,
                     },
                     viaIR: true,
                 },

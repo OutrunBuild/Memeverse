c4c6f76141f82761ceecaa68f0cb51be0488b847
Jason <2045330878@qq.com>
2025-03-02 21:44:32 +0800

    support new network

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,32 +1,91 @@
 source ../.env
 
-# forge script MemeverseScript.s.sol:MemeverseScript --rpc-url bsc_testnet \
-#     --with-gas-price 3000000000 \
-#     --optimize --optimizer-runs 10000 \
-#     --via-ir \
-#     --broadcast --ffi -vvvv \
-#     --verify
+forge script MemeverseScript.s.sol:MemeverseScript --rpc-url bsc_testnet \
+    --with-gas-price 3000000000 \
+    --optimize --optimizer-runs 10000 \
+    --via-ir \
+    --broadcast --ffi -vvvv \
+    --verify
 
-# forge script MemeverseScript.s.sol:MemeverseScript --rpc-url base_sepolia \
-#     --with-gas-price 1200000 \
-#     --optimize --optimizer-runs 10000 \
-#     --via-ir \
-#     --broadcast --ffi -vvvv \
-#     --verify
+forge script MemeverseScript.s.sol:MemeverseScript --rpc-url base_sepolia \
+    --with-gas-price 1200000 \
+    --optimize --optimizer-runs 10000 \
+    --via-ir \
+    --broadcast --ffi -vvvv \
+    --verify
 
-# forge script MemeverseScript.s.sol:MemeverseScript --rpc-url scroll_sepolia \
-#     --priority-gas-price 1000 --with-gas-price 600000000 \
-#     --optimize --optimizer-runs 10000 \
-#     --via-ir \
-#     --broadcast --ffi -vvvv \
-#     --verify
+forge script MemeverseScript.s.sol:MemeverseScript --rpc-url arbitrum_sepolia \
+    --with-gas-price 100000000 \
+    --optimize --optimizer-runs 10000 \
+    --via-ir \
+    --broadcast --ffi -vvvv \
+    --verify
 
-# forge script MemeverseScript.s.sol:MemeverseScript --rpc-url blast_sepolia \
-#     --priority-gas-price 300 --with-gas-price 1200000 \
-#     --optimize --optimizer-runs 10000 \
-#     --via-ir \
-#     --broadcast --ffi -vvvv \
-#     --verify
+forge script MemeverseScript.s.sol:MemeverseScript --rpc-url avalanche_fuji \
+    --priority-gas-price 1000000001 --with-gas-price 1000000001 \
+    --optimize --optimizer-runs 10000 \
+    --via-ir \
+    --broadcast --ffi -vvvv \
+    --verify
+
+forge script MemeverseScript.s.sol:MemeverseScript --rpc-url polygon_amoy \
+    --with-gas-price 40000000000 \
+    --optimize --optimizer-runs 10000 \
+    --via-ir \
+    --broadcast --ffi -vvvv \
+    --verify
+
+forge script MemeverseScript.s.sol:MemeverseScript --rpc-url sonic_blaze \
+    --priority-gas-price 10 --with-gas-price 1000000000 \
+    --optimize --optimizer-runs 10000 \
+    --via-ir \
+    --broadcast --ffi -vvvv \
+    --verify
+
+forge script MemeverseScript.s.sol:MemeverseScript --rpc-url optimistic_sepolia \
+    --with-gas-price 1000000 \
+    --optimize --optimizer-runs 10000 \
+    --via-ir \
+    --broadcast --ffi -vvvv \
+    --verify
+
+forge script MemeverseScript.s.sol:MemeverseScript --rpc-url zksync_sepolia \
+    --with-gas-price 25000000 \
+    --optimize --optimizer-runs 10000 \
+    --via-ir \
+    --broadcast --ffi -vvvv \
+    --verify
+
+forge script MemeverseScript.s.sol:MemeverseScript --rpc-url linea_sepolia \
+    --with-gas-price 250000000 \
+    --optimize --optimizer-runs 10000 \
+    --via-ir \
+    --broadcast --ffi -vvvv \
+    --verify
+    
+forge script MemeverseScript.s.sol:MemeverseScript --rpc-url blast_sepolia \
+    --priority-gas-price 300 --with-gas-price 1200000 \
+    --optimize --optimizer-runs 10000 \
+    --via-ir \
+    --broadcast --ffi -vvvv \
+    --verify
+
+forge script MemeverseScript.s.sol:MemeverseScript --rpc-url scroll_sepolia \
+    --priority-gas-price 1000 --with-gas-price 600000000 \
+    --optimize --optimizer-runs 10000 \
+    --via-ir \
+    --broadcast --ffi -vvvv \
+    --verify
+
+forge script MemeverseScript.s.sol:MemeverseScript --rpc-url monad_testnet \
+    --with-gas-price 52000000000 \
+    --optimize --optimizer-runs 10000 \
+    --via-ir \
+    --broadcast --ffi -vvvv \
+    --verify --verifier sourcify \
+    --verifier-url 'https://sourcify-api-monad.blockvision.org'
+
+
 
 # forge script TestScript.s.sol:TestScript --rpc-url bsc_testnet \
 #     --with-gas-price 3000000000 \
@@ -42,9 +101,9 @@ source ../.env
 #     --broadcast --ffi -vvvv \
 #     --verify
 
-forge script TestScript.s.sol:TestScript --rpc-url base_sepolia \
-    --with-gas-price 1200000 \
-    --optimize --optimizer-runs 10000 \
-    --via-ir \
-    --broadcast --ffi -vvvv \
-    --verify
+# forge script TestScript.s.sol:TestScript --rpc-url base_sepolia \
+#     --with-gas-price 1200000 \
+#     --optimize --optimizer-runs 10000 \
+#     --via-ir \
+#     --broadcast --ffi -vvvv \
+#     --verify

1bf0447333e7c1b0d60720aff4a33b80e6a6a7e3
Jason <2045330878@qq.com>
2025-03-03 23:01:22 +0800

    add flash mint

--- src/token/MemeLiquidProof.sol
+++ src/token/MemeLiquidProof.sol
@@ -6,11 +6,12 @@ import { Nonces } from "@openzeppelin/contracts/utils/Nonces.sol";
 import { IMemeLiquidProof } from "./interfaces/IMemeLiquidProof.sol";
 import { OutrunERC20PermitInit } from "../common/OutrunERC20PermitInit.sol";
 import { OutrunERC20Init, OutrunERC20Votes } from "../common/governance/OutrunERC20Votes.sol";
+import { IERC3156FlashLender, IERC3156FlashBorrower } from "../common/IERC3156FlashLender.sol";
 
 /**
  * @title Omnichain Memeverse Proof Of Liquidity Token
  */
-contract MemeLiquidProof is IMemeLiquidProof, OutrunERC20PermitInit, OutrunERC20Votes {
+contract MemeLiquidProof is IMemeLiquidProof, IERC3156FlashLender, OutrunERC20PermitInit, OutrunERC20Votes {
     address public memecoin;
     address public memeverseLauncher;
 
@@ -75,5 +76,63 @@ contract MemeLiquidProof is IMemeLiquidProof, OutrunERC20PermitInit, OutrunERC20
 
     function nonces(address owner) public view override(OutrunERC20PermitInit, Nonces) returns (uint256) {
         return super.nonces(owner);
+    }
+
+    /**************************************************
+     ********************* Flash Mint *****************
+     **************************************************/
+    bytes32 private constant RETURN_VALUE = keccak256("ERC3156FlashBorrower.onFlashLoan");
+    uint256 public constant flashloanFeeRate = 25;   // 0.25 %.
+
+    /**
+     * @dev Returns the maximum amount of tokens available for loan.
+     * @param token The address of the token that is requested.
+     * @return The amount of token that can be loaned.
+     */
+    function maxFlashLoan(address token) public view virtual returns (uint256) {
+        return token == address(this) ? _maxSupply() - totalSupply() : 0;
+    }
+
+    /**
+     * @dev Returns the fee applied when doing flash loans.
+     * @param token The token to be flash loaned.
+     * @param value The amount of tokens to be loaned.
+     * @return The fees applied to the corresponding flash loan.
+     */
+    function flashFee(address token, uint256 value) public view virtual returns (uint256) {
+        if (token != address(this)) {
+            revert ERC3156UnsupportedToken(token);
+        }
+        return value * flashloanFeeRate / 10000;
+    }
+
+    /**
+     * @dev Performs a flash loan. New tokens are minted and sent to the `receiver`, who is required to 
+     * implement the {IERC3156FlashBorrower} interface. By the end of the flash loan, the receiver is 
+     * expected to own value + fee tokens so they can be burned.
+     * @param receiver The receiver of the flash loan. Should implement the {IERC3156FlashBorrower-onFlashLoan} interface.
+     * @param token The token to be flash loaned. Only `address(this)` is supported.
+     * @param value The amount of tokens to be loaned.
+     * @param data An arbitrary datafield that is passed to the receiver.
+     * @return `true` if the flash loan was successful.
+     */
+    function flashLoan(
+        IERC3156FlashBorrower receiver,
+        address token,
+        uint256 value,
+        bytes calldata data
+    ) public virtual returns (bool) {
+        uint256 maxLoan = maxFlashLoan(token);
+        require(value <= maxLoan, ERC3156ExceededMaxLoan(maxLoan));
+
+        uint256 fee = flashFee(token, value);
+        _mint(address(receiver), value);
+        require(
+            receiver.onFlashLoan(msg.sender, token, value, fee, data) == RETURN_VALUE, 
+            ERC3156InvalidReceiver(address(receiver))
+        );
+        
+        _burn(address(receiver), value + fee);
+        return true;
     }
 }

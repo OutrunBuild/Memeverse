b4e4307d7bd6c527e0ab3af8e95ed4bedd25df7e
Jason <2045330878@qq.com>
2025-03-04 23:28:11 +0800

    delete flash mint

--- src/token/Memecoin.sol
+++ src/token/Memecoin.sol
@@ -5,12 +5,11 @@ import { IMemecoin } from "./interfaces/IMemecoin.sol";
 import { OutrunOFTInit } from "../common/layerzero/oft/OutrunOFTInit.sol";
 import { IMemeverseLauncher } from "../verse/interfaces/IMemeverseLauncher.sol";
 import { IMemecoinYieldVault } from "../yield/interfaces/IMemecoinYieldVault.sol";
-import { IERC3156FlashLender, IERC3156FlashBorrower } from "../common/IERC3156FlashLender.sol";
 
 /**
  * @title Omnichain Memecoin
  */
-contract Memecoin is IMemecoin, OutrunOFTInit, IERC3156FlashLender {
+contract Memecoin is IMemecoin, OutrunOFTInit {
     address public memeverseLauncher;
 
     /**
@@ -54,72 +53,5 @@ contract Memecoin is IMemecoin, OutrunOFTInit, IERC3156FlashLender {
         address msgSender = msg.sender;
         require(balanceOf(msgSender) >= amount, InsufficientBalance());
         _burn(msgSender, amount);
-    }
-
-    /**************************************************
-     ********************* Flash Mint *****************
-     **************************************************/
-    bytes32 private constant RETURN_VALUE = keccak256("ERC3156FlashBorrower.onFlashLoan");
-    uint256 public constant flashloanFeeRate = 25;   // 0.25 %.
-
-    /**
-     * @dev Returns the maximum amount of tokens available for loan.
-     * @param token The address of the token that is requested.
-     * @return The amount of token that can be loaned.
-     */
-    function maxFlashLoan(address token) public view virtual returns (uint256) {
-        return token == address(this) ? type(uint256).max - totalSupply() : 0;
-    }
-
-    /**
-     * @dev Returns the fee applied when doing flash loans.
-     * @param token The token to be flash loaned.
-     * @param value The amount of tokens to be loaned.
-     * @return The fees applied to the corresponding flash loan.
-     */
-    function flashFee(address token, uint256 value) public view virtual returns (uint256) {
-        require(token == address(this), ERC3156UnsupportedToken(token));
-
-        return value * flashloanFeeRate / 10000;
-    }
-
-    /**
-     * @dev Performs a flash loan. New tokens are minted and sent to the `receiver`, who is required to 
-     * implement the {IERC3156FlashBorrower} interface. By the end of the flash loan, the receiver is 
-     * expected to own value + fee tokens so they can be burned.
-     * @param receiver The receiver of the flash loan. Should implement the {IERC3156FlashBorrower-onFlashLoan} interface.
-     * @param token The token to be flash loaned. Only `address(this)` is supported.
-     * @param value The amount of tokens to be loaned.
-     * @param data An arbitrary datafield that is passed to the receiver.
-     * @return `true` if the flash loan was successful.
-     */
-    function flashLoan(
-        IERC3156FlashBorrower receiver,
-        address token,
-        uint256 value,
-        bytes calldata data
-    ) public virtual returns (bool) {
-        uint256 maxLoan = maxFlashLoan(token);
-        require(value <= maxLoan, ERC3156ExceededMaxLoan(maxLoan));
-
-        uint256 fee = flashFee(token, value);
-        address receiverAddress = address(receiver);
-        _mint(receiverAddress, value);
-        require(
-            receiver.onFlashLoan(msg.sender, token, value, fee, data) == RETURN_VALUE, 
-            ERC3156InvalidReceiver(receiverAddress)
-        );
-        
-        address yieldVault = IMemeverseLauncher(memeverseLauncher).getYieldVaultByMemecoin(address(this));
-        if (fee == 0 || yieldVault == address(0)) {
-            _burn(receiverAddress, value + fee);
-        } else {
-            _burn(receiverAddress, value);
-            _transfer(receiverAddress, address(this), fee);
-            IMemecoinYieldVault(yieldVault).accumulateYields(fee);
-        }
-
-        emit MemecoinFlashLoan(receiverAddress, value, fee, data);
-        return true;
     }
 }
